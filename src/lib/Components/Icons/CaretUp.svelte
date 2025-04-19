b91fc26d2c66bcff95833187d71b10b328940468
André Sarandeses <acamposarandeses@gmail.com>
2025-04-20 00:23:23 +0100

    Prototyping new element button

---
+++ src/lib/Components/Icons/CaretUp.svelte
@@ -0,0 +1,11 @@
+<svg
+	data-v-534830e1=""
+	data-v-45f352e3=""
+	class="Icon CaretUp"
+	viewBox="0 0 10 7"
+	xmlns="http://www.w3.org/2000/svg"
+	fill-rule="evenodd"
+    fill="currentColor"
+>
+	<path d="m0 5 2 2 3 -3 3 3 2 -2-5-5-5 5Z"></path>
+</svg>
---
+++ src/lib/SettingPanel/ElementButton/ElementButton.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+	import CaretDown from '$icons/CaretDown.svelte';
+	import CaretUp from '$icons/CaretUp.svelte';
+
+	let selected: boolean = false;
+
+	function clickCb() {
+		selected = !selected;
+	}
+</script>
+
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<div class="element-button-wrapper">
+	<div class="reorder-buttons">
+		<button class="form-button reorder-button move-up">
+			<CaretUp />
+		</button>
+		<button class="form-button reorder-button move-down">
+			<CaretDown />
+		</button>
+	</div>
+	<div class="constraints-ui" class:clickable={true} class:selected on:click={clickCb}>
+		<div class="header">
+			<div class="icon-container"></div>
+			<div class="element-name">Test Constraint</div>
+		</div>
+		{#if selected}
+			<div class="editor-wrapper">
+				<div class="editor">Test</div>
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style lang="scss">
+	.element-button-wrapper {
+		display: flex;
+	}
+
+	.reorder-buttons {
+		flex-grow: 0;
+		min-width: 0;
+	}
+
+	.form-button {
+		// form button styling
+		color: #333;
+		background: #fff;
+		border: 0.125rem solid black;
+		border-radius: 0.15rem;
+		font-weight: 700;
+		line-height: 1;
+		min-height: 2em;
+		min-width: 6ch;
+		padding: 0 0.5em;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		transition: background 0.1s;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		user-select: none;
+	}
+
+	.reorder-button {
+		position: relative;
+		min-width: 0;
+		width: 2rem !important;
+		min-height: 0;
+		height: 1.5rem;
+
+		&.move-up {
+			border-bottom-left-radius: 0;
+			border-bottom-right-radius: 0;
+            border-bottom: 0.05rem solid black;
+		}
+
+		&.move-down {
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+            border-top: 0.05rem solid black;
+		}
+	}
+
+	.constraints-ui {
+		flex: 1;
+		min-width: 0;
+
+		border: 0.125rem solid;
+		border-radius: 0.25rem;
+		transition: all 0.2s ease-in-out;
+
+		&.selected {
+			box-shadow: 0 0.1em 0.2em -0.1em #333;
+			outline: var(--input-outline-color) solid 0.3rem;
+		}
+	}
+
+	.header {
+		display: flex;
+		align-items: center;
+		position: relative;
+		gap: 0.5rem;
+
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		user-select: none;
+	}
+
+	.icon-container {
+		width: 2.5rem;
+		height: 2.5rem;
+		overflow: hidden;
+		flex-shrink: 0;
+	}
+
+	.element-name {
+		line-height: 1;
+		flex: 1;
+	}
+
+	.editor-wrapper {
+		height: auto;
+		margin: 0 0.5rem;
+	}
+
+	.editor {
+		border-top: 0.0625rem solid black;
+		padding: 0.5rem 0;
+	}
+</style>
