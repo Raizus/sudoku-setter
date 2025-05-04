5df7e5c93442c58aa573f07bdabc10537f1f81c8
André Sarandeses <acamposarandeses@gmail.com>
2025-05-05 00:11:21 +0100

    Added change to not render editor when there's nothing  to render

--- src/lib/SettingPanel/ElementsPanel/ElementEditor.svelte
+++ src/lib/SettingPanel/ElementsPanel/ElementEditor.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { squareCellElementHandlers } from '$src/lib/Puzzle/ElementsInfo/SquareCellElementHandlers';
-	import type { TOOLID } from '$src/lib/Puzzle/Tools';
+	import { isLocalConstraint, type TOOLID } from '$src/lib/Puzzle/Tools';
 	import ConstraintCheckbox from './ConstraintCheckbox.svelte';
 	import ConstraintList from './ConstraintList.svelte';
 	import ToolModeButtons from './ToolModeButtons.svelte';
@@ -8,25 +8,33 @@
 	export let tool_id: TOOLID;
 	const element_info = squareCellElementHandlers[tool_id];
 	const negative_constraints = element_info.negative_constraints;
+	const has_negatives =
+		negative_constraints && Object.keys(negative_constraints).length > 0 ? true : false;
+	const is_local = isLocalConstraint(tool_id);
+	// don't render editor if there's nothing to render
+	// i.e global constraints without negative_constraints
+	const render = is_local || has_negatives;
 </script>
 
-<div class="editor-wrapper">
-	<div class="editor">
-		<div class="editor-layout">
-			{#if negative_constraints}
-				{#each negative_constraints as neg_const}
-					<ConstraintCheckbox
-						{tool_id}
-						neg_tool_id={neg_const.toolId}
-						description={neg_const.description}
-					/>
-				{/each}
-			{/if}
-			<ToolModeButtons {tool_id} />
-			<ConstraintList {tool_id} />
+{#if render}
+	<div class="editor-wrapper">
+		<div class="editor">
+			<div class="editor-layout">
+				{#if negative_constraints}
+					{#each negative_constraints as neg_const}
+						<ConstraintCheckbox
+							{tool_id}
+							neg_tool_id={neg_const.toolId}
+							description={neg_const.description}
+						/>
+					{/each}
+				{/if}
+				<ToolModeButtons {tool_id} />
+				<ConstraintList {tool_id} />
+			</div>
 		</div>
 	</div>
-</div>
+{/if}
 
 <style lang="scss">
 	.editor-wrapper {
