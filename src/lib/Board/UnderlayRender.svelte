7e40437ca1735d5cd05c68be300a7aebf38ebeed
André Sarandeses <acamposarandeses@gmail.com>
2025-05-13 18:32:47 +0100

    Minor refactor

--- src/lib/Board/UnderlayRender.svelte
+++ src/lib/Board/UnderlayRender.svelte
@@ -26,46 +26,36 @@
 	$: enable_fog_mask = $enableFogMaskStore;
 </script>
 
-<g class="underlay-layer" mask={enable_fog_mask ? "url(#fog-mask-fog)" : null}>
+<g class="underlay-layer" mask={enable_fog_mask ? 'url(#fog-mask-fog)' : null}>
 	{#each underlay_elements as element}
 		{#if element.constraints}
-			{#if isSingleCellTool(element.tool_id)}
-				<g class="element-group" data-toolId={element.tool_id}>
+			<g class="element-group" data-toolId={element.tool_id}>
+				{#if isSingleCellTool(element.tool_id)}
 					{#each Object.entries(element.constraints) as entry (entry[0])}
 						<SingleCellToolRender tool={entry[1] as SingleCellTool} c_id={entry[0]} />
 					{/each}
-				</g>
-			{:else if isLineTool(element.tool_id)}
-				<g class="element-group" data-toolId={element.tool_id}>
+				{:else if isLineTool(element.tool_id)}
 					{#each Object.entries(element.constraints) as entry (entry[0])}
 						<LineToolRender tool={entry[1] as LineToolI} c_id={entry[0]} />
 					{/each}
-				</g>
-			{:else if isArrowTool(element.tool_id)}
-				<g class="element-group" data-toolId={element.tool_id}>
+				{:else if isArrowTool(element.tool_id)}
 					{#each Object.entries(element.constraints) as entry (entry[0])}
 						<ArrowToolRender tool={entry[1] as ArrowToolI} c_id={entry[0]} />
 					{/each}
-				</g>
-			{:else if isCageTool(element.tool_id)}
-				<g class="element-group" data-toolId={element.tool_id}>
+				{:else if isCageTool(element.tool_id)}
 					{#each Object.entries(element.constraints) as entry (entry[0])}
 						<CageToolRender tool={entry[1] as CageToolI} c_id={entry[0]} />
 					{/each}
-				</g>
-			{:else if isCloneTool(element.tool_id)}
-				<g class="element-group" data-toolId={element.tool_id}>
+				{:else if isCloneTool(element.tool_id)}
 					{#each Object.entries(element.constraints) as entry (entry[0])}
 						<CloneToolRender tool={entry[1] as CloneToolI} c_id={entry[0]} />
 					{/each}
-				</g>
-			{:else if isOutsideDirectionTool(element.tool_id)}
-				<g class="element-group" data-toolId={element.tool_id}>
+				{:else if isOutsideDirectionTool(element.tool_id)}
 					{#each Object.entries(element.constraints) as entry (entry[0])}
 						<OutsideDirectionToolRender tool={entry[1] as OutsideDirectionToolI} c_id={entry[0]} />
 					{/each}
-				</g>
-			{/if}
+				{/if}
+			</g>
 		{/if}
 	{/each}
 </g>
