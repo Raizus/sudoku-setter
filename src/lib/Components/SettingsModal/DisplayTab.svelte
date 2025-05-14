c017bf63893611b3e69f66d389db7cef5feda93e
André Sarandeses <acamposarandeses@gmail.com>
2025-05-14 01:42:40 +0100

    Minor change

--- src/lib/Components/SettingsModal/DisplayTab.svelte
+++ src/lib/Components/SettingsModal/DisplayTab.svelte
@@ -8,21 +8,21 @@
 <div class="settings-tab-layout">
 	<div class="grid">
 		<SettingsColorSelector
-			name="Non-given value color"
+			name="Non-given value color:"
 			value={settings.non_given_color}
 			onChangeCb={(value) => {
 				updateSettingsValue('non_given_color', value);
 			}}
 		/>
 		<SettingsColorSelector
-			name="Seen cells color"
+			name="Seen cells color:"
 			value={settings.seen_cells_color}
 			onChangeCb={(value) => {
 				updateSettingsValue('seen_cells_color', value);
 			}}
 		/>
 		<SettingsColorSelector
-			name="Selection color"
+			name="Selection color:"
 			value={settings.selection_color}
 			onChangeCb={(value) => {
 				updateSettingsValue('selection_color', value);
