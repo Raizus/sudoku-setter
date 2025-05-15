0b9177badc2d3f51ce28dfcb81e3c33740d149ae
André Sarandeses <acamposarandeses@gmail.com>
2025-05-15 16:20:20 +0100

    Changed button title for Add Element button

--- src/lib/SettingPanel/Subpanel/ToolPanelHeader.svelte
+++ src/lib/SettingPanel/Subpanel/ToolPanelHeader.svelte
@@ -12,7 +12,7 @@
 
 <PanelHeader {title} bind:isOpen>
 	<!-- svelte-ignore a11y_consider_explicit_label -->
-	<button slot="add-button" class="add-button" title="Add Constraint" on:click|stopPropagation={() => onOpenCallback()}>
+	<button slot="add-button" class="add-button" title="Add Element" on:click|stopPropagation={() => onOpenCallback()}>
 		<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 96 960 960"
 			><path
 				d="M453 776h60V610h167v-60H513V376h-60v174H280v60h173v166Zm27.266 200q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80 658.319 80 575.5q0-82.819 31.5-155.659Q143 347 197.5 293t127.341-85.5Q397.681 176 480.5 176q82.819 0 155.659 31.5Q709 239 763 293t85.5 127Q880 493 880 575.734q0 82.734-31.5 155.5T763 858.316q-54 54.316-127 86Q563 976 480.266 976Z"
