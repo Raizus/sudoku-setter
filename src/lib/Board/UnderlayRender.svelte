f1dbc9543b8fdbd644f1a05c1beb9e5fcc3bc7b1
André Sarandeses <acamposarandeses@gmail.com>
2025-11-19 19:23:39 +0000

    Updated UnderlayRender to use a registry of renderers instead

--- src/lib/Board/UnderlayRender.svelte
+++ src/lib/Board/UnderlayRender.svelte
@@ -6,14 +6,9 @@
 		isCloneTool,
 		isLineTool,
 		isOutsideDirectionTool,
-		isSingleCellTool
+		isSingleCellTool,
+		type TOOLID
 	} from '../Puzzle/Tools';
-		import { type SingleCellTool } from "../Puzzle/puzzle_schema";
-		import { type LineToolI } from "../Puzzle/puzzle_schema";
-		import { type ArrowToolI } from '../Puzzle/puzzle_schema';
-		import { type CageToolI } from "../Puzzle/puzzle_schema";
-		import { type CloneToolI } from "../Puzzle/puzzle_schema";
-		import { type OutsideDirectionToolI } from "../Puzzle/puzzle_schema";
 	import SingleCellToolRender from './Constraints/SingleCellToolRender.svelte';
 	import LineToolRender from './Constraints/LineToolRender.svelte';
 	import ArrowToolRender from './Constraints/ArrowToolRender.svelte';
@@ -22,6 +17,19 @@
 	import OutsideDirectionToolRender from './Constraints/OutsideDirectionToolRender.svelte';
 	import { enableFogMaskStore } from '$stores/BoardStore';
 
+	const toolRenderers = [
+		{ check: isSingleCellTool, component: SingleCellToolRender },
+		{ check: isLineTool, component: LineToolRender },
+		{ check: isArrowTool, component: ArrowToolRender },
+		{ check: isCageTool, component: CageToolRender },
+		{ check: isCloneTool, component: CloneToolRender },
+		{ check: isOutsideDirectionTool, component: OutsideDirectionToolRender }
+	];
+
+	function getToolRenderer(toolId: TOOLID) {
+		return toolRenderers.find((renderer) => renderer.check(toolId))?.component;
+	}
+
 	$: underlay_elements = $underlayElementsStore;
 	$: enable_fog_mask = $enableFogMaskStore;
 </script>
@@ -29,30 +37,11 @@
 <g class="underlay-layer" mask={enable_fog_mask ? 'url(#fog-mask-fog)' : null}>
 	{#each underlay_elements as element}
 		{#if !element.disabled && element.constraints}
+			{@const ToolComponent = getToolRenderer(element.tool_id)}
 			<g class="element-group" data-toolId={element.tool_id}>
-				{#if isSingleCellTool(element.tool_id)}
+				{#if ToolComponent}
 					{#each Object.entries(element.constraints) as entry (entry[0])}
-						<SingleCellToolRender tool={entry[1] as SingleCellTool} c_id={entry[0]} />
-					{/each}
-				{:else if isLineTool(element.tool_id)}
-					{#each Object.entries(element.constraints) as entry (entry[0])}
-						<LineToolRender tool={entry[1] as LineToolI} c_id={entry[0]} />
-					{/each}
-				{:else if isArrowTool(element.tool_id)}
-					{#each Object.entries(element.constraints) as entry (entry[0])}
-						<ArrowToolRender tool={entry[1] as ArrowToolI} c_id={entry[0]} />
-					{/each}
-				{:else if isCageTool(element.tool_id)}
-					{#each Object.entries(element.constraints) as entry (entry[0])}
-						<CageToolRender tool={entry[1] as CageToolI} c_id={entry[0]} />
-					{/each}
-				{:else if isCloneTool(element.tool_id)}
-					{#each Object.entries(element.constraints) as entry (entry[0])}
-						<CloneToolRender tool={entry[1] as CloneToolI} c_id={entry[0]} />
-					{/each}
-				{:else if isOutsideDirectionTool(element.tool_id)}
-					{#each Object.entries(element.constraints) as entry (entry[0])}
-						<OutsideDirectionToolRender tool={entry[1] as OutsideDirectionToolI} c_id={entry[0]} />
+						<svelte:component this={ToolComponent} tool={entry[1] as any} c_id={entry[0]} />
 					{/each}
 				{/if}
 			</g>
