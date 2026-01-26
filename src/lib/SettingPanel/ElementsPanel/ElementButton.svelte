999c7a0b8d6feccdbc518e994469330aac68634a
André Sarandeses <acamposarandeses@gmail.com>
2026-01-26 18:58:54 +0000

    Added UI to reorder elements, but still need to change schema and functions for feature to work properly

--- src/lib/SettingPanel/ElementsPanel/ElementButton.svelte
+++ src/lib/SettingPanel/ElementsPanel/ElementButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	// import CaretDown from '$icons/CaretDown.svelte';
-	// import CaretUp from '$icons/CaretUp.svelte';
+	import CaretDown from '$icons/CaretDown.svelte';
+	import CaretUp from '$icons/CaretUp.svelte';
 	import Trash from '$icons/Trash.svelte';
 	import { getToolInfo, type AbstractElementHandlers } from '$src/lib/Puzzle/ElementHandlersUtils';
 	import { TOOLS, type TOOLID } from '$src/lib/Puzzle/Tools';
@@ -21,6 +21,7 @@
 	import ElementEditor from './ElementEditor.svelte';
 	import MoreButton from './MoreButton.svelte';
 
+	export let order: number | undefined = undefined;
 	export let tool_id: TOOLID;
 	export let elementHandlers: AbstractElementHandlers;
 
@@ -79,20 +80,26 @@
 		return description;
 	}
 
+	function moveUp() {}
+
+	function moveDown() {}
+
 	$: selected = tool_id === $toolStore;
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <div class="element-button-wrapper">
-	<!-- <div class="reorder-buttons">
-		<button class="form-button reorder-button move-up">
-			<CaretUp />
-		</button>
-		<button class="form-button reorder-button move-down">
-			<CaretDown />
-		</button>
-	</div> -->
+	{#if order !== undefined}
+		<div class="reorder-buttons">
+			<button class="form-button reorder-button move-up" on:click={moveUp}>
+				<CaretUp />
+			</button>
+			<button class="form-button reorder-button move-down" on:click={moveDown}>
+				<CaretDown />
+			</button>
+		</div>
+	{/if}
 	<div
 		class="constraints-ui"
 		class:clickable={true}
@@ -129,35 +136,34 @@
 		display: flex;
 	}
 
-	// .reorder-buttons {
-	// 	flex-grow: 0;
-	// 	min-width: 0;
-	// }
-
-	// .reorder-button {
-	// 	color: #333;
-	// 	background: #fff;
-	// 	border: 0.125rem solid black;
-	// 	font-weight: 700;
-	// 	line-height: 1;
-	// 	position: relative;
-	// 	min-width: 0;
-	// 	width: 2rem !important;
-	// 	min-height: 0;
-	// 	height: 1.5rem;
-
-	// 	&.move-up {
-	// 		border-bottom-left-radius: 0;
-	// 		border-bottom-right-radius: 0;
-	// 		border-bottom: 0.05rem solid black;
-	// 	}
-
-	// 	&.move-down {
-	// 		border-top-left-radius: 0;
-	// 		border-top-right-radius: 0;
-	// 		border-top: 0.05rem solid black;
-	// 	}
-	// }
+	.reorder-buttons {
+		flex-grow: 0;
+		min-width: 0;
+	}
+	.reorder-button {
+		background: var(--button-hover-background-color);
+		border: 0.1rem solid black;
+		font-weight: 700;
+		line-height: 1;
+		position: relative;
+		min-width: 0;
+		width: 2rem !important;
+		min-height: 0;
+		height: 1.5rem;
+		&:hover {
+			background-color: var(--button2-hover-background-color);
+		}
+		&.move-up {
+			border-bottom-left-radius: 0;
+			border-bottom-right-radius: 0;
+			border-bottom: 0.05rem solid black;
+		}
+		&.move-down {
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+			border-top: 0.05rem solid black;
+		}
+	}
 
 	.constraints-ui {
 		flex: 1;
