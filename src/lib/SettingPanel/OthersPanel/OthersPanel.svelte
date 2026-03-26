11025d0265bd817d847f62ee88a4691e842adcde
André Sarandeses <acamposarandeses@gmail.com>
2026-03-26 19:37:11 +0000

    Changed Set Solution button class/styling

--- src/lib/SettingPanel/OthersPanel/OthersPanel.svelte
+++ src/lib/SettingPanel/OthersPanel/OthersPanel.svelte
@@ -14,7 +14,7 @@
 	<PanelHeader slot="panel-header" title="Other Tools" bind:isOpen />
 	<svelte:fragment slot="panel-content">
 		<button
-			class="entry-panel-button"
+			class="panel-button"
 			on:click={setSolution}
 			title="Set current cell values as the puzzle solution"
 		>
@@ -23,19 +23,3 @@
 	</svelte:fragment>
 </Panel>
 
-<style lang="scss">
-	.entry-panel-button {
-		position: relative;
-		// border: 1px solid var(--panel-radio-border-color);
-		border: 1px solid transparent;
-		border-radius: 4px;
-		background-color: transparent;
-		cursor: pointer;
-		height: 2rem;
-		text-align: left;
-
-		&:hover {
-			background-color: var(--panel-radio-background-hover);
-		}
-	}
-</style>
