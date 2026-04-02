bbb8b40b3fc97a301f1728ddd9bebfc902a6ee41
André Sarandeses <acamposarandeses@gmail.com>
2026-04-02 20:17:43 +0100

    Changed Outline filter back

--- src/lib/Board/Constraints/SingleCellMultiArrowRenderAux.svelte
+++ src/lib/Board/Constraints/SingleCellMultiArrowRenderAux.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-	import { defaultSingleCellMultiArrowShape } from '$lib/Puzzle/Shape/Shape';
-	import { getDefaultShape } from '$lib/Puzzle/ElementHandlersUtils';
-	import { elementInfoRegistry } from '$src/lib/Puzzle/ElementsInfo/ElementInfoRegistry';
+	import { defaultSingleCellMultiArrowShape, type ShapeI } from '$lib/Puzzle/Shape/Shape';
 	import { getSingleCellMultiArrowPath } from '$lib/utils/SquareCellGridRenderUtils';
 	import type { CellMultiArrowToolI } from '$src/lib/Puzzle/puzzle_schema';
 
 	export let tool: CellMultiArrowToolI;
-
-	$: defaultShape =
-		getDefaultShape(tool.toolId, elementInfoRegistry) ?? defaultSingleCellMultiArrowShape;
-	$: shape = tool.shape ?? defaultShape;
+	export let shape: ShapeI = defaultSingleCellMultiArrowShape;
 </script>
 
 {#each tool.directions as direction}
--- src/lib/Board/Constraints/SingleCellMultiArrowToolGroupRender.svelte
+++ src/lib/Board/Constraints/SingleCellMultiArrowToolGroupRender.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { getDefaultShape } from '$src/lib/Puzzle/ElementHandlersUtils';
+	import { elementInfoRegistry } from '$src/lib/Puzzle/ElementsInfo/ElementInfoRegistry';
 	import type { CellMultiArrowToolI } from '$src/lib/Puzzle/puzzle_schema';
+	import { defaultSingleCellMultiArrowShape } from '$src/lib/Puzzle/Shape/Shape';
 	import SingleCellMultiArrowRenderAux from './SingleCellMultiArrowRenderAux.svelte';
-	import { getCurrentConstraintStore, getOutlineFilterUrl } from './utils';
+	import { getCurrentConstraintStore } from './utils';
 
 	export let tool: CellMultiArrowToolI;
 	export let c_id: string | undefined = undefined;
@@ -11,12 +14,32 @@
 
 	$: currentConstraintId = $currentConstraintStore?.id;
 	$: is_selected = c_id !== undefined && c_id === currentConstraintId;
-	$: filter_url = getOutlineFilterUrl(outline, is_selected);
+
+	$: defaultShape =
+		getDefaultShape(tool.toolId, elementInfoRegistry) ?? defaultSingleCellMultiArrowShape;
+	$: shape = tool.shape ?? defaultShape;
+
+	$: outlineShape = {
+		...shape,
+		stroke: 'var(--grid-background-color)',
+		strokeWidth: shape.strokeWidth ? shape.strokeWidth + 0.06 : 0.06
+	};
+	$: selectedOutlineShape = {
+		...shape,
+		stroke: 'var(--constraint-selected-color)',
+		strokeWidth: shape.strokeWidth ? shape.strokeWidth + 0.07 : 0.07
+	};
 </script>
 
 {#if c_id !== undefined}
-	<g class="single-cell-multi-arrow-tool" data-id={`${c_id}`} filter={filter_url}>
-		<SingleCellMultiArrowRenderAux {tool} />
+	<g class="single-cell-multi-arrow-tool" data-id={`${c_id}`}>
+		{#if outline}
+			<SingleCellMultiArrowRenderAux {tool} shape={outlineShape} />
+		{/if}
+		{#if is_selected}
+			<SingleCellMultiArrowRenderAux {tool} shape={selectedOutlineShape} />
+		{/if}
+		<SingleCellMultiArrowRenderAux {tool} {shape} />
 	</g>
 {:else}
 	<g class="single-cell-multi-arrow-constraint-preview" opacity={0.5}>
